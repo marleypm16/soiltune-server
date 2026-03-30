5db861031eb99731326f9ec144498c043e81d920
Marley Martins <yarlempm16@gmail.com>
2026-03-30 20:29:33 +0000

    feat : add main function

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+type SensorData struct {
+	SensorID    string  `json:"sensor_id"`
+	Temperature float64 `json:"temperature"`
+	Humidity    float64 `json:"humidity"`
+	Weight      float64 `json:"weight"`
+}
+
+func main() {
+	messageHandler := influxDBHandler()
+	mqttHandler(messageHandler)
+}
