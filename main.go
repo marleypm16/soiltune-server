f6c8b799e988c1f3d35d1de31d383cbfd4dad818
Marley Martins <yarlempm16@gmail.com>
2026-04-01 12:58:52 +0000

    refactor : mudando imports e chamadas de funções

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-type SensorData struct {
-	SensorID    string  `json:"sensor_id"`
-	Temperature float64 `json:"temperature"`
-	Humidity    float64 `json:"humidity"`
-	Weight      float64 `json:"weight"`
-}
+import (
+	"soiltune-consumer/influxdb"
+	"soiltune-consumer/mqttHandler"
+)
 
 func main() {
-	messageHandler := influxDBHandler()
-	mqttHandler(messageHandler)
+	influxdb.InfluxDBHandler()
+	messageHandler := influxdb.InfluxDBHandler()
+	mqttHandler.MqttHandler(messageHandler)
 }
